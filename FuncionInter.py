98bc25f7de56bb2f810b0d5d9a5f50e210bdae34
Juliocotzo <Juliocotzo11@gmail.com>
2021-01-05 21:20:25 -0600

    feat: Selected Interfaz

--- FuncionInter.py
+++ FuncionInter.py
@@ -23,7 +23,7 @@ class Intermedio():
 
 	def procesar_funcion0(self):
 		global instrucciones_Global,tc_global1,ts_global1,listaErrores,erroressss,ts_globalIndex1
-		instrucciones = g.parse('CREATE DATABASE Prueba1;')
+		instrucciones = g.parse('CREATE DATABASE DBFase2;')
 		erroressss = ErrorHTML()
 		if  erroressss.getList()== []:
 			instrucciones_Global = instrucciones
@@ -41,61 +41,7 @@ class Intermedio():
 
 	def procesar_funcion1(self):
 		global instrucciones_Global,tc_global1,ts_global1,listaErrores,erroressss,ts_globalIndex1
-		instrucciones = g.parse('USE Prueba1;')
-		erroressss = ErrorHTML()
-		if  erroressss.getList()== []:
-			instrucciones_Global = instrucciones
-			ts_global = TS.TablaDeSimbolos()
-			ts_globalIndex = TSINDEX.TablaDeSimbolos()
-			tc_global = TC.TablaDeTipos()
-			tc_global1 = tc_global
-			ts_global1 = ts_global
-			ts_globalIndex1 = ts_globalIndex
-			salida = procesar_instrucciones(instrucciones, ts_global,tc_global,ts_globalIndex)
-			return salida
-		else:
-			return 'Parser Error'
-
-
-	def procesar_funcion2(self):
-		global instrucciones_Global,tc_global1,ts_global1,listaErrores,erroressss,ts_globalIndex1
-		instrucciones = g.parse('CREATE TABLE TABLA (  id  INTEGER  , NOMBRE  INTEGER  , APELLIDO  INTEGER   );')
-		erroressss = ErrorHTML()
-		if  erroressss.getList()== []:
-			instrucciones_Global = instrucciones
-			ts_global = TS.TablaDeSimbolos()
-			ts_globalIndex = TSINDEX.TablaDeSimbolos()
-			tc_global = TC.TablaDeTipos()
-			tc_global1 = tc_global
-			ts_global1 = ts_global
-			ts_globalIndex1 = ts_globalIndex
-			salida = procesar_instrucciones(instrucciones, ts_global,tc_global,ts_globalIndex)
-			return salida
-		else:
-			return 'Parser Error'
-
-
-	def procesar_funcion3(self):
-		global instrucciones_Global,tc_global1,ts_global1,listaErrores,erroressss,ts_globalIndex1
-		instrucciones = g.parse(' CREATE INDEX IDXNOMBRE ON TABLA  (  id,NOMBRE,APELLIDO  ) ;')
-		erroressss = ErrorHTML()
-		if  erroressss.getList()== []:
-			instrucciones_Global = instrucciones
-			ts_global = TS.TablaDeSimbolos()
-			ts_globalIndex = TSINDEX.TablaDeSimbolos()
-			tc_global = TC.TablaDeTipos()
-			tc_global1 = tc_global
-			ts_global1 = ts_global
-			ts_globalIndex1 = ts_globalIndex
-			salida = procesar_instrucciones(instrucciones, ts_global,tc_global,ts_globalIndex)
-			return salida
-		else:
-			return 'Parser Error'
-
-
-	def procesar_funcion4(self):
-		global instrucciones_Global,tc_global1,ts_global1,listaErrores,erroressss,ts_globalIndex1
-		instrucciones = g.parse(' CREATE INDEX IDXNOMBRE1 ON TABLA  (  NOMBRE  ) ;')
+		instrucciones = g.parse('USE DBFase2;')
 		erroressss = ErrorHTML()
 		if  erroressss.getList()== []:
 			instrucciones_Global = instrucciones
